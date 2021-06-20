2b46b272ff124d4ba864c28dd1343013b777a148
judithbisset <77537313+judithbisset@users.noreply.github.com>
2021-06-20 16:54:00 +0200

    fixed icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 
 export default function WeatherInfo(props) {
@@ -19,7 +20,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              {props.data.icon}
+              <WeatherIcon code={props.data.icon} />
             </div>
 
             <div className="float-left">
